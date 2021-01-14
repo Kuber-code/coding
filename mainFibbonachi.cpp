901c937b8c8975d7a3fd801f0c0ffe67ad344b5e
Kuber-code <kuba.lichosik@gmail.com>
2021-01-14 13:30:42 +0100

    Create mainFibbonachi.cpp
    added new file

---
+++ mainFibbonachi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getNFibonnaciElement(int n){
+    std::vector <int> vector (n+1);
+    int elemnt;
+    vector[0] = 1;
+    vector[1] = 1;
+    for (int i = 2; i < n; i++){
+        vector[i] = vector[i-1] + vector[i-2];
+    }
+    element = vector[n-1];
+    return element;
+}
+
+sort()
+// sortowanie kubełkwoe - sprawdzic
+// przetwarzanie obrazu;
+
+int main(){
+getNFibonnaciElement(); 
+return 0;
+}
